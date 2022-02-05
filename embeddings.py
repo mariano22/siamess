9c443c591c7fc32b74a1aaf7b3e5c9431e739f99
Mariano Crosetti <marianocrosetti1993@gmail.com>
2022-02-05 20:58:43 -0300

    update

--- embeddings.py
+++ embeddings.py
@@ -3,24 +3,24 @@ from tqdm import tqdm
 
 class SingleDS(torch.utils.data.Dataset):
     """ Dataset of single samples """
-    def __init__(self, fns, get_x, get_y, cls):
-        self.fns,self.get_x,self.get_y,self.cls = fns,get_x,get_y,cls
+    def __init__(self, fns, get_x, get_y, vocab):
+        self.fns,self.get_x,self.get_y,self.vocab = fns,get_x,get_y,vocab
 
     def __getitem__(self, i):
-        return ( self.get_x(self.fns[i]), self.cls[self.get_y(self.fns[i])] )
+        return self.get_x(self.fns[i]), tensor(self.vocab[self.get_y(self.fns[i])])
 
     def __len__(self): return len(self.fns)
 
-def get_embeddings(my_dl, m):
+def get_all_embeddings(dl, m):
     with torch.no_grad():
         features_list = []
         y_list = []
-        for x,y in tqdm(my_dl):
+        for x,y in tqdm(dl):
             features = m.encoder(x)
             features = nn.Flatten()(features)
             features_list.append(features)
             y_list.append(y)
-        return torch.cat(features_list), torch.cat(y_list)
+    return torch.cat(features_list), torch.cat(y_list)
 
 class Embeddings:
     def __init__(self, ds, name, bs=256):
